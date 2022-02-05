60ba433721f9ed8017130c0b35037e87653ddc52
vaibhavss08 <81666621+vaibhavss08@users.noreply.github.com>
2022-02-06 01:26:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ driver = webdriver.Chrome(executable_path= os.environ.get("CHROMEDRIVER_PATH"), 
 driver.get("https://www.bseindia.com/corporates/corporates_act.html")
 driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
 
+
 soup = BSoup(driver.page_source, 'html.parser')
 dom = etree.HTML(str(soup))
 
@@ -32,30 +33,70 @@ rows = 1 + len(dom.xpath("/html/body/div[5]/div/div/div/table[1]/tbody/tr/td/div
 stock=[]
 ex_date=[]
 purpose=[]
+result = []
 
-replace = '!?_@#$%^&*.,-'
+replace = '!?_@#$%^*.,-'
+
+flag = False
 
 for r in range(2, rows + 1):
     try:
         date = (dom.xpath("/html/body/div[5]/div/div/div/table[1]/tbody/tr/td/div/table/tbody/tr[" + str(r) + "]/td[3]")[0].text)
 
-        for i in range(25):
-            d = (datetime.now() + timedelta(i)).strftime("%d " "%b " "%Y")
+        if flag or datetime.now() <= datetime.strptime(date,"%d %b %Y"):
+            stock_name = (dom.xpath(
+                "/html/body/div[5]/div/div/div/table[1]/tbody/tr/td/div/table/tbody/tr[" + str(r) + "]/td[2]")[0].text)
+            stock_name = stock_name.strip()
+            for k in replace:
+                stock_name = stock_name.replace(k, "")
 
-            if(date.strip() == d):
-                stock_name = (dom.xpath("/html/body/div[5]/div/div/div/table[1]/tbody/tr/td/div/table/tbody/tr[" + str(r) + "]/td[2]")[0].text)
-                stock_name = stock_name.strip()
-                for k in replace:
-                    stock_name = stock_name.replace(k, "")
-
-                stock.append(stock_name)
-                purpose.append(dom.xpath("/html/body/div[5]/div/div/div/table[1]/tbody/tr/td/div/table/tbody/tr[" + str(r) + "]/td[4]")[0].text)
-                ex_date.append(d)
+            stock.append(stock_name)
+            purpose.append(dom.xpath(
+                "/html/body/div[5]/div/div/div/table[1]/tbody/tr/td/div/table/tbody/tr[" + str(r) + "]/td[4]")[0].text)
+            ex_date.append(date)
+            flag = True
+        else:
+            continue
     except:
         continue
 
 
-driver.quit()
+driver.get("https://www.bseindia.com/corporates/Forth_Results.html")
+
+soup = BSoup(driver.page_source, 'html.parser')
+dom = etree.HTML(str(soup))
+
+rows = 1 + len(dom.xpath("/html/body/div[5]/div/div/div/table/tbody/tr/td/table/tbody/tr"))
+flag = True
+stock_result= []
+result_date = []
+
+
+for r in range(2, rows):
+   stock_result.append(dom.xpath("/html/body/div[5]/div/div/div/table/tbody/tr/td/table/tbody/tr["+str(r)+"]/td[2]")[0].text)
+   result_date.append(dom.xpath("/html/body/div[5]/div/div/div/table/tbody/tr/td/table/tbody/tr["+str(r)+"]/td[3]")[0].text)
+
+
 results = pd.DataFrame(list(zip(stock, ex_date, purpose)), columns=['Stock', 'Ex-Date', 'Purpose'])
+
+list = pd.read_csv("bse.csv", encoding='latin-1')
+
+id = []
+
+for i in stock:
+    cond = list['Security Name'] == i
+    id.append(list['Security Id'].loc[cond].str.cat(sep='\n'))
+
+stock_re = []
+
+for i in id:
+    try:
+        stock_re.append(result_date[stock_result.index(i)])
+    except:
+        stock_re.append("-")
+
+results['Result Date'] = stock_re
+
+pd.set_option('display.max_colwidth', None)
 results.index+=1
 st.table(results)
